96fe84797b6a9ad8290059f8eec41c55ab78e64a
limenglei01 <limenglei@bytedance.com>
2023-07-25 14:52:59 +0800

    add template deploy

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # douyincloud-nodejs-koa-demo
-本项目是抖音云平台基于 Nodejs [koa](https://koajs.com/) 框架的开发模版，模版通过使用 Redis 和 MongoDB 实现了简单的 hello-world 功能。\
+本项目是抖音云平台基于 Nodejs [koa](https://koajs.com/) 框架的开发模版，模版实现了简单的云调用与获取openID的功能。\
 抖音云平台支持基于 Git 代码和 Docker 镜像部署两种方式。其中，Dockerfile 文件可以参考本项目中的 Dockerfile 文件。
 部署在抖音云平台的服务日志需要重定向到标准输出，并在抖音云平台日志功能中查看。
 
@@ -18,65 +18,32 @@
 前往抖音云托管平台「调试」功能界面，进行请求调试。
 
 ## API说明
-### `POST /api/set_data_to_redis?key=test`
-调用 redis 组件设置数据，其中 key 通过 query 传递，value 在 HTTP request body中传递
-### 请求参数
-- `value`:`string` 写入redis的数据
+### `GET /api/get_open_id`
+获取openid，当未绑定用户时会返回获取openid失败的错误
 
 
 ### 响应结果
 ```json
 {
-    "success": true,
+    "err_no": 0,
+    "err_msg": "success",
+    "data": "719f****-****-4c**-a0**-*********"
 }
 ```
 
-### `GET /api/get_data_from_redis?key=test`
-调用 redis 组件获取 value
+### `POST /api/text/antidirt`
+云调用示例，调用抖音开放平台的OpenApi进行脏词检测
+
 ### 请求参数
-- `key`:`string` redis key
+- `content`:`string` 待检测的内容
 
 ### 响应结果
 ```json
 {
-    "success": true,
-    "data": "",
+    "err_no": 0,
+    "err_msg": "success",
+    "data": ""
 }
-```
-
-### `POST /api/set_data_to_mongodb?name=test`
-调用 mongodb 组件写入数据
-### 请求参数
-- `name`:`string` 写入mongodb的数据
-
-
-### 响应结果
-```json
-{
-    "success": true,
-}
-```
-
-### `GET /api/get_data_from_mongodb?name=test`
-调用 mongodb 组件获取数据
-### 请求参数
-- `name`:`string` 
-
-### 响应结果
-```json
-{
-    "success": true,
-    "data": {},
-}
-```
-
-### 组件使用注意事项
-在抖音云托管平台上启用组件后，抖音云平台会自动将组件的地址，账号，密码以环境变量的方式注入到容器中。\
-以Redis为例，在抖音云托管平台启用Redis组件后，平台会生成 `REDIS_ADDRESS`，`REDIS_USERNAME`，`REDIS_PASSWORD`三个环境变量，在业务代码中可以使用如下代码获取相应值。
-```js
-	const redisAddr = process.env.REDIS_ADDRESS;
-	const redisUserName = process.env.REDIS_USERNAME;
-	const redisPassword = process.env.REDIS_PASSWORD; 
 ```
 
 ## License
--- src/server.ts
+++ src/server.ts
@@ -1,106 +1,44 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import Router from '@koa/router'
-import Redis from 'ioredis';
-import mongoose from 'mongoose';
-import assert from "assert";
-
-// 初始化各服务的连接 redis, mongo
-async function initService() {
-    const {REDIS_ADDRESS, REDIS_USERNAME, REDIS_PASSWORD, MONGO_ADDRESS, MONGO_USERNAME, MONGO_PASSWORD} = process.env;
-    const [ REDIS_HOST, REDIS_PORT] = REDIS_ADDRESS.split(':');
-    const redis = new Redis({
-        port: parseInt(REDIS_PORT, 10),
-        host: REDIS_HOST,
-        username: REDIS_USERNAME,
-        password: REDIS_PASSWORD,
-        db: 0,
-    });
-
-    assert(await redis.echo('echo') === 'echo', `redis echo error`);
-
-    const mongoUrl = `mongodb://${MONGO_USERNAME}:${encodeURIComponent(MONGO_PASSWORD)}@${MONGO_ADDRESS}`;
-    await mongoose.connect(mongoUrl);    
-
-    return {
-        redis,
-        mongoose,
+import axios from 'axios';
+const app = new Koa();
+const router = new Router();
+router.get('/', ctx => {
+    ctx.body = `Nodejs koa demo project`;
+}).get('/api/get_open_id', async (ctx) => {
+    const value = ctx.request.header['x-tt-openid'] as string;
+    if (value) {
+        ctx.body = {
+            success: true,
+            data: value,
+        }
+    } else {
+        ctx.body = {
+            success: false,
+            message: `dyc-open-id not exist`,
+        }
     }
-}
+}).post('/api/text/antidirt', async (ctx) => {
+    const body: any = ctx.request.body;
+    const content = body.content;
+    const res = await axios.post('http://developer.toutiao.com/api/v2/tags/text/antidirt', {
+        "tasks": [
+          {
+            "content": content
+          }
+        ]
+      });
+    ctx.body = {
+        "result": res.data,
+        "success": true,
+    }
+});
 
-initService().then(async ({ redis, mongoose}) => {
-    const kittySchema = new mongoose.Schema({
-        name: String
-    });
+app.use(bodyParser());
+app.use(router.routes());
 
-    const Kitten = mongoose.model('Kitten', kittySchema);
-
-    const app = new Koa();
-
-    const router = new Router();
-    router.get('/', ctx => {
-        ctx.body = `Nodejs koa demo project`;
-    }).get('/api/get_data_from_redis', async(ctx) => {
-        const key = ctx.query.key as string;
-        assert(key?.trim(), `key is required`);
-        const value = await redis.get(key);
-        if (value) {
-            ctx.body = {
-                success: true,
-                data: value,
-            }
-        } else {
-            ctx.status = 404;
-            ctx.body = {
-                success: false,
-                message: `${key} not exist`,
-            }
-        }
-    }).post('/api/set_data_to_redis', async(ctx) => {
-        const key = ctx.query.key as string;
-        const body: any = ctx.request.body;
-        const value = body.value as string;
-        assert(key?.trim(), `key is required`);
-        assert(value?.trim(), `value is required`);
-        await redis.set(key, value);
-        ctx.body = {
-            success: true,
-        }
-    }).get('/api/get_data_from_mongodb', async(ctx) => {
-        const name = ctx.query.name as string;
-        assert(name?.trim(), `name is required`);
-        const data = await Kitten.findOne({ name});
-        
-        if (data) {
-            ctx.body = {
-                success: true,
-                data: data.toJSON(),
-            }
-        } else {
-            ctx.status = 404;
-            ctx.body = {
-                success: false,
-                message: `${name} not exist`,
-            }
-        }
-    }).post('/api/set_data_to_mongodb', async(ctx) => {
-        const name = ctx.query.name as string;
-        assert(name?.trim(), `name is required`);
-
-        const kit = new Kitten({ name });
-        await kit.save();
-
-        ctx.body = {
-            success: true,
-        }
-    });
-
-    app.use(bodyParser());
-    app.use(router.routes());
-
-    const PORT = 8000;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-
-}).catch((error: string) => console.log("Init service  error: ", error));
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
